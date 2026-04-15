e950e59ca38c461a81b41803b8a308953edb2442
Stéphane Raynaud <stephane.raynaud@hardis-group.com>
2026-04-15 15:27:22 +0200

    fix Module build failed: UnhandledSchemeError: Reading from "node:path" is not handled by plugins (Unhandled scheme).

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,21 @@ const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
     "*": ["public/**/*", ".next/static/**/*"],
   },
+  webpack: (config, { webpack }) => {
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    config.externals["node:fs"] = "commonjs node:fs";
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    config.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(/^node:/, (resource) => {
+        resource.request = resource.request.replace(/^node:/, "");
+      })
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
